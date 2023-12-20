d9f47389cfad36ff37f9a1a65027a703f1d72a07
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2023-12-21 01:32:04 +0200

    The Python FastAPI Implementation Files

---
+++ alembic/versions/036aa392dfb0_auto_vote.py
@@ -0,0 +1,27 @@
+"""auto-vote
+
+Revision ID: 036aa392dfb0
+Revises: 036d0a4565b7
+Create Date: 2021-08-29 23:41:21.054819
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = '036aa392dfb0'
+down_revision = '036d0a4565b7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('votes',
+    sa.Column('user_id', sa.Integer(), nullable=False),
+    sa.Column('post_id', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['post_id'], ['posts.id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
+    sa.PrimaryKeyConstraint('user_id', 'post_id')
+    )
+
+def downgrade():
+    op.drop_table('votes')
