ce4102d35379b7db9fe9e167e82c1ac7985bb14e
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2023-12-21 01:32:46 +0200

    The Python FastAPI Implementation Files

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from . import models
+from .database import engine
+from .routers import post, user, auth, vote
+from .config import settings
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(vote.router)
+
+
+@app.get("/")
+def root():
+    return { "message": "Hello World, This is my message" }
+
