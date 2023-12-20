69f57de2e002dc36ef66e3340998c67a19f64763
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2023-12-21 01:33:42 +0200

    The Python FastAPI Implementation Files

---
+++ tests/test_users.py
@@ -0,0 +1,37 @@
+import pytest
+from jose import jwt
+from app import schemas
+
+from app.config import settings
+
+def test_create_user(client):
+    res = client.post("/users/", json={"email": "hello123@gmail.com", "password": "password123"})
+
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello123@gmail.com"
+    assert res.status_code == 201
+
+
+def test_login_user(test_user, client):
+    res = client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('sanjeev@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('sanjeev@gmail.com', None, 422)
+])
+
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+
