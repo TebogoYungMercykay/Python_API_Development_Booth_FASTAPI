ccec5e650f6df97d89133a3862b174e4c88c359c
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2023-12-21 18:49:27 +0200

    The Python FastAPI Implementation Files

--- alembic/versions/036aa392dfb0_auto_vote.py
+++ alembic/versions/036aa392dfb0_auto_vote.py
@@ -2,7 +2,7 @@
 
 Revision ID: 036aa392dfb0
 Revises: 036d0a4565b7
-Create Date: 2021-08-29 23:41:21.054819
+Create Date: 2023-12-21 22:50:42.028498
 
 """
 from alembic import op
@@ -16,11 +16,11 @@ depends_on = None
 
 def upgrade():
     op.create_table('votes',
-    sa.Column('user_id', sa.Integer(), nullable=False),
-    sa.Column('post_id', sa.Integer(), nullable=False),
-    sa.ForeignKeyConstraint(['post_id'], ['posts.id'], ondelete='CASCADE'),
-    sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
-    sa.PrimaryKeyConstraint('user_id', 'post_id')
+        sa.Column('user_id', sa.Integer(), nullable=False),
+        sa.Column('post_id', sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(['post_id'], ['posts.id'], ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('user_id', 'post_id')
     )
 
 def downgrade():
