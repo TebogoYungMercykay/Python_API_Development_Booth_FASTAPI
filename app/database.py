2c367d0fff64667122c1dccacff7c4db8584179f
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2023-12-21 06:48:33 +0200

    The Python FastAPI Implementation Files

--- app/database.py
+++ app/database.py
@@ -1,9 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 from .config import settings
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
