a24b6cd1b1a897bb8f6248c84515bc554ede5212
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2023-12-22 11:54:44 +0200

    The Python FastAPI Implementation Files

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -13,6 +13,5 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
-
 
 settings = Settings()
