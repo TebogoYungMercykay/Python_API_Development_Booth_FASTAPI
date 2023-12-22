f08b8d62b35408cb0270b7f5adf93bb2ec587c80
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2023-12-22 16:11:46 +0200

    The Python FastAPI Implementation Files

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -24,3 +24,7 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     access_token = oauth2.create_access_token(data={"user_id": user.id})
 
     return {"access_token": access_token, "token_type": "bearer"}
+
+@router.post('/logout')
+def logout():
+    return { "message": "Functionality Under Construction." }
