8efb509fd24724f0e5a34c01a597592d86ad80a3
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2023-12-22 16:11:57 +0200

    The Python FastAPI Implementation Files

---
+++ app/routers/disease_prediction.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from typing import List, Optional
+
+from sqlalchemy import func
+from .. import models, schemas, oauth2
+from ..database import get_db
+
+MESSAGE_UNDER_CONSTRUCTION = "Functionality Under Construction."
+
+router = APIRouter(
+    prefix="/disease_prediction",
+    tags=['Disease Prediction Model']
+)
+
+@router.get('/')
+def checkdisease():
+    return { "message": MESSAGE_UNDER_CONSTRUCTION }
+
+
+@router.post('/checkdisease')
+def checkdisease():
+    return { "message": MESSAGE_UNDER_CONSTRUCTION }
+
