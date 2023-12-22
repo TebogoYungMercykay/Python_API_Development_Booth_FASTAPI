e99483f0a9eda345707a5dc36648c36bf291cc67
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2023-12-22 14:28:53 +0200

    The Python FastAPI Implementation Files

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -21,9 +21,6 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
 
-    # create a token
-    # return token
-
     access_token = oauth2.create_access_token(data={"user_id": user.id})
 
     return {"access_token": access_token, "token_type": "bearer"}
--- app/routers/user.py
+++ app/routers/user.py
@@ -5,13 +5,12 @@ from ..database import get_db
 
 router = APIRouter(
     prefix="/users",
-    tags=['Users']
+    tags=['Accounts']
 )
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
-    # hash the password - user.password
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
 
