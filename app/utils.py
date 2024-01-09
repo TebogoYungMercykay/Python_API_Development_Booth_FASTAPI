b8e75d6a3584b3b7ea8e4be12be36fcd6547943b
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-01-10 00:44:00 +0200

    Added to the utils, A Method for Disease List

--- app/utils.py
+++ app/utils.py
@@ -44,3 +44,13 @@ def get_symptoms_list():
         'yellow_crust_ooze'
     ]
     
+def get_disease_list():
+    return [
+        'Fungal infection', 'Allergy', 'GERD', 'Chronic cholestasis', 'Drug Reaction', 'Peptic ulcer diseae', 'AIDS', 'Diabetes ',
+        'Gastroenteritis', 'Bronchial Asthma', 'Hypertension ', 'Migraine', 'Cervical spondylosis', 'Paralysis (brain hemorrhage)',
+        'Jaundice', 'Malaria', 'Chicken pox', 'Dengue', 'Typhoid', 'hepatitis A', 'Hepatitis B', 'Hepatitis C', 'Hepatitis D',
+        'Hepatitis E', 'Alcoholic hepatitis', 'Tuberculosis', 'Common Cold', 'Pneumonia', 'Dimorphic hemmorhoids(piles)',
+        'Heart attack', 'Varicose veins', 'Hypothyroidism', 'Hyperthyroidism', 'Hypoglycemia', 'Osteoarthristis',
+        'Arthritis', '(vertigo) Paroymsal  Positional Vertigo', 'Acne', 'Urinary tract infection', 'Psoriasis', 'Impetigo'
+    ]
+    
