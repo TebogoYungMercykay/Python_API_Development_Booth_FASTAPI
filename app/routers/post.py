66fa750fa5902c69cff912f4a5178412b579fcff
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-01-10 23:44:10 +0200

    Returning a Well Structured JSON Response

--- app/routers/post.py
+++ app/routers/post.py
@@ -14,7 +14,7 @@ router = APIRouter(
     tags=['Posts']
 )
 
-@router.get("/", response_model=List[schemas.PostOut])
+@router.get("/", response_model=schemas.JSONListPostOut)
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
 
     posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
@@ -29,9 +29,9 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
         }
         return JSONResponse(content=error_response, status_code=404)
     
-    return posts
+    return schemas.JSONListPostOut(status="success", id=current_user.id, data=posts)
 
-@router.get("/all_posts", response_model=List[schemas.AllPostOut])
+@router.get("/all_posts", response_model=schemas.JSONAllPostOut)
 def all_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     posts_query = db.query(models.Post, func.count(models.Vote.post_id).label("votes"))\
         .outerjoin(models.Vote, models.Vote.post_id == models.Post.id)\
@@ -59,10 +59,10 @@ def all_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
         post_out = schemas.AllPostOut(Post=post, votes=votes, replies=replies_dict.get(post.id, []))
         all_posts_out_list.append(post_out)
 
-    return all_posts_out_list
+    return schemas.JSONAllPostOut(status="success", id=current_user.id, data=all_posts_out_list)
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.JSONPost)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
     new_post = models.Post(owner_id=current_user.id, **post.dict())
@@ -70,10 +70,10 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     db.commit()
     db.refresh(new_post)
 
-    return new_post
+    return schemas.JSONPost(status="success", id=current_user.id, data=new_post)
 
 
-@router.get("/{id}", response_model=schemas.PostOut)
+@router.get("/{id}", response_model=schemas.JSONPostOut)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
     post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
@@ -87,7 +87,7 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
         }
         return JSONResponse(content=error_response, status_code=404)
 
-    return post
+    return schemas.JSONPost(status="success", id=current_user.id, data=post)
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -115,10 +115,15 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depe
     post_query.delete(synchronize_session=False)
     db.commit()
 
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    content_response = {
+        "status": "success",
+        "id": -1,
+        "data": "No Content"
+    }
+    return JSONResponse(content=content_response, status_code=204)
 
 
-@router.put("/{id}", response_model=schemas.Post)
+@router.put("/{id}", response_model=schemas.JSONPost)
 def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
     post_query = db.query(models.Post).filter(models.Post.id == id)
@@ -142,11 +147,11 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
+    
+    return schemas.JSONPost(status="success", id=current_user.id, data=post_query.first())
 
-    return post_query.first()
 
-
-@router.post('/create_reply/{id}', response_model=schemas.RepliesData)
+@router.post('/create_reply/{id}', response_model=schemas.JSONRepliesData)
 def create_reply(id: int, reply_post: schemas.Replies, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     doctor = db.query(models.Doctor).filter(models.Doctor.doctor_id == current_user.id).first()
     if not doctor or id != reply_post.post_id:
@@ -171,9 +176,52 @@ def create_reply(id: int, reply_post: schemas.Replies, db: Session = Depends(get
     db.commit()
     db.refresh(new_post)
     
-    
-    return {
-            "reply": new_post,
-            "doctor": doctor
-        }
+    response_obj = {"reply": new_post, "doctor": doctor }
+    return schemas.JSONRepliesData(status="success", id=current_user.id, data=response_obj)
 
+
+@router.post("/vote", status_code=status.HTTP_201_CREATED)
+def vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        error_response = {
+            "status": "error",
+            "id": -1,
+            "data": f"Post with id: {vote.post_id} does not exist"
+        }
+        return JSONResponse(content=error_response, status_code=404)
+
+    vote_query = db.query(models.Vote).filter(
+        models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
+
+    found_vote = vote_query.first()
+    if (vote.dir == 1):
+        if found_vote:
+            error_response = {
+                "status": "error",
+                "id": -1,
+                "data": f"user {current_user.id} has alredy voted on post {vote.post_id}"
+            }
+            return JSONResponse(content=error_response, status_code=409)
+        
+        new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
+        db.add(new_vote)
+        db.commit()
+        
+        return {"status": "success", "id": current_user.id, "data": "successfully added vote"}
+    
+    else:
+        if not found_vote:
+            error_response = {
+                "status": "error",
+                "id": -1,
+                "data": "Vote does not exist"
+            }
+            return JSONResponse(content=error_response, status_code=404)
+
+        vote_query.delete(synchronize_session=False)
+        db.commit()
+
+        return {"status": "success", "id": current_user.id, "data": "successfully deleted vote"}
+    
