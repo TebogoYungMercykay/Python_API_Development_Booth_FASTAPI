5e12be5e965945c5cc815b50d989c67267f1bb08
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-01-10 22:52:54 +0200

    Returning a Well Structured JSON Response

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from .. import schemas, database, models, oauth2
-
+from fastapi.responses import JSONResponse
 
 router = APIRouter(
     prefix="/vote",
@@ -14,8 +14,12 @@ def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_use
 
     post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
     if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with id: {vote.post_id} does not exist")
+        error_response = {
+            "status": "error",
+            "id": -1,
+            "data": f"Post with id: {vote.post_id} does not exist"
+        }
+        return JSONResponse(content=error_response, status_code=404)
 
     vote_query = db.query(models.Vote).filter(
         models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
@@ -23,18 +27,27 @@ def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_use
     found_vote = vote_query.first()
     if (vote.dir == 1):
         if found_vote:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                detail=f"user {current_user.id} has alredy voted on post {vote.post_id}")
+            error_response = {
+                "status": "error",
+                "id": -1,
+                "data": f"user {current_user.id} has alredy voted on post {vote.post_id}"
+            }
+            return JSONResponse(content=error_response, status_code=409)
+        
         new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
         db.add(new_vote)
         db.commit()
         return {"message": "successfully added vote"}
     else:
         if not found_vote:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail="Vote does not exist")
+            error_response = {
+                "status": "error",
+                "id": -1,
+                "data": "Vote does not exist"
+            }
+            return JSONResponse(content=error_response, status_code=404)
 
         vote_query.delete(synchronize_session=False)
         db.commit()
 
-        return {"message": "successfully deleted vote"}
+        return {"status": "success", "id": current_user.id, "data": "successfully deleted vote"}
