bbce14499ddba589bd0179b22c26b144c36adcda
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-01-10 21:40:22 +0200

    Email Confirmation & User Registration

--- app/routers/user.py
+++ app/routers/user.py
@@ -8,22 +8,26 @@ router = APIRouter(
     tags=['Accounts']
 )
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+# @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
-    hashed_password = utils.hash(user.password)
-    user.password = hashed_password
+#     hashed_password = utils.hash(user.password)
+#     user.password = hashed_password
 
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+#     new_user = models.User(**user.dict())
+#     db.add(new_user)
+#     db.commit()
+#     db.refresh(new_user)
 
-    return new_user
+#     return new_user
 
 @router.post("/signup_patient", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def signup_patient(user: schemas.PatientCreate, db: Session = Depends(get_db)):
-
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Email already registered.")
+    
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
     
@@ -41,7 +45,11 @@ def signup_patient(user: schemas.PatientCreate, db: Session = Depends(get_db)):
 
 @router.post("/signup_doctor", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def signup_doctor(user: schemas.DoctorCreate, db: Session = Depends(get_db)):
-
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Email already registered.")
+    
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
     
